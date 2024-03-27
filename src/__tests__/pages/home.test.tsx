2230b1bdcafabb94c2d346581991cf7fce156dbd
Nurul Furqon <furqon.mascode@gmail.com>
2024-03-27 18:22:14 +0700

    Initial commit

---
+++ src/__tests__/pages/home.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import { expect, test } from 'vitest'
+
+import Home from '@/app/page'
+
+test('Home', () => {
+  render(<Home />)
+  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
+})
